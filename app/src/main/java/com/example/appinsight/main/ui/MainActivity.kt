1422ba334614bad839af0cc68b8a3099c82ec9f8
ИÐIgor Starkov <Kazesteam@yandex.ru>
2025-04-10 13:46:58 +0300

    AI - 001: Настраивает проект

--- app/src/main/java/com/example/appinsight/main/ui/MainActivity.kt
+++ app/src/main/java/com/example/appinsight/main/ui/MainActivity.kt
@@ -1,12 +1,14 @@
-package com.example.appinsight
+package com.example.appinsight.main.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.appinsight.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
